48dc01ed70ed4ae831891d043bd863e6745dde4f
musawerpopal <mnsrpopal1@gamil.com>
2024-07-14 23:22:11 -0700

    browser handling

--- src/test/java/Crossbrowsing/Crossbrowsinghandling.java
+++ src/test/java/Crossbrowsing/Crossbrowsinghandling.java
@@ -5,9 +5,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Scanner;
+
 public class Crossbrowsinghandling {
     public static void main(String[] args) {
-        String browsertype = "firefox";
+        String browsertype;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("choose browser: chrome, firefox, edge");
+         browsertype = scanner.next();
         WebDriver driver;
 
         if(browsertype.equals("chrome")) {
